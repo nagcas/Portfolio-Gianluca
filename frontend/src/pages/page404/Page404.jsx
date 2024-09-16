e3f9995b2c6011b97e7a4ff349041dd4b6f422ac
nagcas <studio.nagcas@outlook.it>
2024-09-16 23:34:59 +0200

    update translation 404

--- frontend/src/pages/page404/Page404.jsx
+++ frontend/src/pages/page404/Page404.jsx
@@ -2,17 +2,21 @@ import "./Page404.css";
 
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 function Page404() {
+
+  const { t  } = useTranslation('global');
+
   return (
     <Container className="page__not__found">
       <div className="content">
         <h1>404</h1>
-        <h4>Oops! La pagina che stai cercando non esiste.</h4>
-        <p>Sembra che tu abbia preso una strada sbagliata nel mio portfolio...</p>
-        <p>Non preoccuparti, ti aiuto a tornare al profilo. 😊</p>
+        <h4>{t("page404.text-1")}</h4>
+        <p>{t("page404.text-2")}</p>
+        <p>{t("page404.text-3")}</p>
         <Link to="/" className="home">
-          Torna al profilo
+        {t("page404.link")}
         </Link>
       </div>
     </Container>

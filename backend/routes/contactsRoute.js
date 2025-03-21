bbfcf4d39caf03dbd4eda0c483731e2334424b55
Gianluca <studio.nagcas@outlook.it>
2025-03-21 13:28:29 +0100

    add middlewares

--- backend/routes/contactsRoute.js
+++ backend/routes/contactsRoute.js
@@ -2,16 +2,17 @@ import express from "express";
 import getAllContacts from "../controllers/getAllController.js";
 import postContact from "../controllers/postController.js";
 import deleteContact from "../controllers/deleteController.js";
+import { getKey } from "../middlewares/getKey.js";
 
 const router = express.Router();
 
 // Definizione di una rotta GET per ottenere una lista dei messaggi con paginazione e ordinamento
-router.get("/", getAllContacts);
+router.get("/", getKey, getAllContacts);
 
 // Definizione di una rotta POST per ricevere un messaggio di contatto dalla pagina portfolio
 router.post("/", postContact);
 
 // Definizione di una route DELETE per eliminare un messaggio di un utente
-router.delete("/:contactId", deleteContact);
+router.delete("/:contactId", getKey, deleteContact);
 
 export default router;

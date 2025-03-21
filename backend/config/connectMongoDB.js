5c8372facdfccc2017d812e9c2d0737b60fbb3e2
Gianluca <studio.nagcas@outlook.it>
2025-03-21 11:34:34 +0100

    add connect mongodb

---
+++ backend/config/connectMongoDB.js
@@ -0,0 +1,12 @@
+// Connessione a MongoDB
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export function connectMongo() {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connesso correttamente."))
+    .catch((error) => console.error("MongoDB: errore di connessione.", error));
+}
